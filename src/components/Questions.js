45b3d0ff9def7f3be2f8b68c897f894128959403
Joao Victor Fernandes <jovic258.fernandes@hotmail.com>
2021-08-19 03:03:41 -0300

    second commit

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,27 +1,27 @@
 import React, {useState, useEffect, useRef}from 'react';
 import killua from './images/original.jpg'
 const Questions = ({data, onAsnwerUpdate, numberOfQuestions, activeQuestion, onSetActiveQuestion, onSetStep})=>{
+
+    const[selected, setSelected] = useState('');
+    const[error, setError]=useState('');
+    const radioWrapper = useRef();
+    const changeHandler = (e)=>{
+        setSelected(e.target.value);
+    }
     const nextClickHandler=(e)=>{
 
     }
     return(
         <center>
         <div class ="question">
-            <h2 class ="text-question">Pergunta bla bla bla bla bla bla bla bla</h2>
+            <h2 class ="text-question">{data.question}</h2>
             <img class ="option-image" src = {killua}></img>
             <div class="grid2x2">
-            <div class="box box1"><div>
-            <button class ="option" name="answer" value="">Option 1</button>
+            {data.choices.map((choice, i) =>(
+            <div class="box box"i><div>
+                <button class ="option" name="answer" value="{choice}">{choice}</button>
             </div></div>
-            <div class="box box2"><div>            
-            <button class ="option" name="answer" value="">Option 2</button>
-            </div></div>
-            <div class="box box3"><div>            
-            <button class ="option" name="answer" value="">Option 3</button>
-            </div></div>
-            <div class="box box4"><div>            
-            <button class ="option" name="answer" value="">Option 4</button>
-            </div></div>
+            ))}
             </div>
         </div>
         </center>
