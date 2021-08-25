3417557515a423f7f2495de1c3fd8857ae964a8c
Joao Victor Fernandes <jovic258.fernandes@hotmail.com>
2021-08-25 10:27:51 -0300

    end changes

--- src/components/End.js
+++ src/components/End.js
@@ -2,6 +2,9 @@ import React,{useEffect, useState} from'react';
 import easy_1 from './images/easy_result_higher_5.jpg'
 import easy_2 from './images/easy_result_higher_3.jpg'
 import easy_3 from './images/easy_result_higher_1.jpg'
+import med_1 from './images/med_result_1.jpg'
+import med_2 from './images/med_result_2.jpg'
+import med_3 from './images/med_result_3.jpg'
 const End = ({results, data, diff}) => {
     const [correctAnswers, setCorrectAsnwers] = useState(0);
     useEffect(()=>{
@@ -62,9 +65,53 @@ const End = ({results, data, diff}) => {
             )
         }
     }else if(diff == 2){
-        return(
-            <a>Not done yet</a>
-        )
+        let numOfQuest = 7;
+        if(correctAnswers >= 6){
+            return(
+                <center>
+                    <div class ="buttons-end">
+                    <div class ="button-wrapper-end">
+                    <h1>Seu Resultado</h1>
+                    <h2>Acertos: {correctAnswers}</h2>
+                    <h2>Erros: {numOfQuest- correctAnswers}</h2>
+                    <h2>Dificuldade: Médio</h2>
+                    <h3>Você está pronto pra próxima dificuldade.</h3>
+                    </div>
+                    <img class ="option-image" src ={med_3}></img> 
+                    </div>
+                </center>
+            )         
+        }else if(correctAnswers >= 4 && correctAnswers <= 5){
+            return(
+                <center>
+                    <div class ="buttons-end">
+                    <div class ="button-wrapper-end">
+                    <h1>Seu Resultado</h1>
+                    <h2>Acertos: {correctAnswers}</h2>
+                    <h2>Erros: {numOfQuest- correctAnswers}</h2>
+                    <h2>Dificuldade: Médio</h2>
+                    <h3>Foi quase, que tal tentar novamente?</h3>
+                    </div>
+                    <img class ="option-image" src ={med_2}></img> 
+                    </div>
+                </center>
+            ) 
+        }else{
+            return(         
+                <center>
+                    <div class ="buttons-end">
+                    <div class ="button-wrapper-end">
+                    <h1>Seu Resultado</h1>
+                    <h2>Acertos: {correctAnswers}</h2>
+                    <h2>Erros: {numOfQuest- correctAnswers}</h2>
+                    <h2>Dificuldade: Médio</h2>
+                    <h3>Talvez seja melhor diminuir a dificuldade.</h3>
+                    </div>
+                    <img class ="option-image" src ={med_1}></img> 
+                    </div>
+                </center>
+            )
+        }
     }else if(diff == 3){
         return(
             <a>Not done yet</a>
